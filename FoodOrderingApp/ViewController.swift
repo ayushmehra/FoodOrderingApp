e8a89ce19098bedbcab130bc3e90c8adf6f5c9c8
ayushmehra <mehra1.ayush@gmail.com>
2015-12-09 12:14:09 +0530

    Initial Commit

---
+++ FoodOrderingApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodOrderingApp
+//
+//  Created by ayush mehra on 12/9/15.
+//  Copyright (c) 2015 ayush mehra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
