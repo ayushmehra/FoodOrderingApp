3ca2166b4642a5f63292b2ad3328693fb321d782
ayushmehra <mehra1.ayush@gmail.com>
2015-12-09 13:15:07 +0530

    updated code

---
+++ FoodOrderingApp/Bridging.h
@@ -0,0 +1,13 @@
+//
+//  Bridging.h
+//  FoodOrderingApp
+//
+//  Created by ayush mehra on 12/9/15.
+//  Copyright (c) 2015 ayush mehra. All rights reserved.
+//
+
+#ifndef FoodOrderingApp_Bridging_h
+#define FoodOrderingApp_Bridging_h
+#import <FacebookSDK/FacebookSDK.h>
+
+#endif
--- FoodOrderingApp/ViewController.swift
+++ FoodOrderingApp/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnFacebookLogin(sender: AnyObject) {
+         FB.login(self.handleLogin);
+    }
+    func handleLogin(){
+        println("SUCCESS");
+        FB.getInfo();
+    }
 
 }
 
